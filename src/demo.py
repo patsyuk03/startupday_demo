05798621e237ace2f941f02177a92cc0245bef6c
patsyuk03 <iryna.gurova@gmail.com>
2023-03-11 19:22:45 +0200

    done demo

--- src/demo.py
+++ src/demo.py
@@ -1,16 +1,30 @@
 import rospy, sys, moveit_commander, tf
 from ar_track_alvar_msgs.msg import AlvarMarkers
 
-cubes_ids = [1, 2]
-boxes_ids = [3, 4]
-marker_ids = [1, 2, 3, 4]
-markers = dict()
+boxes_ids = [7, 8]
+cubes_ids = [[0, 1], [2, 4]]
+marker_ids = boxes_ids+cubes_ids[0]+cubes_ids[1]
+found_markers = list()
+markers = dict({
+    boxes_ids[0]: dict({cubes_ids[0][0]:0,cubes_ids[0][1]:0,boxes_ids[0]:0}),
+    boxes_ids[1]: dict({cubes_ids[1][0]:0,cubes_ids[1][1]:0,boxes_ids[1]:0})
+})
 
 def callback(data):
-    global markers
+    global markers, found_markers
+    found_markers = list()
     for marker in data.markers:
-        if marker.id in marker_ids:
-            markers[marker.id] = marker.pose.pose
+        if marker.id in list(markers.keys()):
+            markers[marker.id][marker.id] = marker.pose.pose
+            found_markers.append(marker.id)
+        elif marker.id in list(markers[boxes_ids[0]].keys()):
+            markers[boxes_ids[0]][marker.id] = marker.pose.pose
+            found_markers.append(marker.id)
+        elif marker.id in list(markers[boxes_ids[1]].keys()):
+            markers[boxes_ids[1]][marker.id] = marker.pose.pose
+            found_markers.append(marker.id)
+    
+
 
 class PNPDemo(object):
     def __init__(self):
@@ -20,33 +34,35 @@ class PNPDemo(object):
         self.xarm7 = moveit_commander.MoveGroupCommander("xarm7")
         self.gripper = moveit_commander.MoveGroupCommander("xarm_gripper")
     
-    def Xarm7ToObject(self, id):
+    def movexArm7(self, box_id, cube_id):
         pose_goal = self.xarm7.get_current_pose().pose
+        marker_pose = markers[box_id][cube_id]
 
         marker_orientation = (
-        markers[cubes_ids[id]].orientation.x,
-        markers[cubes_ids[id]].orientation.y,
-        markers[cubes_ids[id]].orientation.z,
-        markers[cubes_ids[id]].orientation.w)
+        marker_pose.orientation.x,
+        marker_pose.orientation.y,
+        marker_pose.orientation.z,
+        marker_pose.orientation.w)
 
-        euler = tf.transformations.euler_from_quaternion(marker_orientation)
-        quaternion = tf.transformations.quaternion_from_euler(3.14 + euler[0], euler[1], euler[2])
+        xarm7_orientation = (
+        pose_goal.orientation.x,
+        pose_goal.orientation.y,
+        pose_goal.orientation.z,
+        pose_goal.orientation.w)
+
+        euler_marker = tf.transformations.euler_from_quaternion(marker_orientation)
+        euler_xarm7 = tf.transformations.euler_from_quaternion(xarm7_orientation)
+
+        quaternion = tf.transformations.quaternion_from_euler(euler_xarm7[0], euler_xarm7[1], euler_marker[2])
 
         pose_goal.orientation.x = quaternion[0]
         pose_goal.orientation.y = quaternion[1]
         pose_goal.orientation.z = quaternion[2]
         pose_goal.orientation.w = quaternion[3]
-        pose_goal.position.x = markers[cubes_ids[id]].position.x
-        pose_goal.position.y = markers[cubes_ids[id]].position.y
 
-        self.xarm7.set_pose_target(pose_goal)
-        plan_success, traj, planning_time, error_code = self.xarm7.plan()
-        self.ExecutePlan(traj)
+        pose_goal.position.x = marker_pose.position.x
+        pose_goal.position.y = marker_pose.position.y
 
-    def Xarm7ToStorage(self, id):
-        pose_goal = self.xarm7.get_current_pose().pose
-        pose_goal.position.x = markers[boxes_ids[id]].position.x
-        pose_goal.position.y = markers[boxes_ids[id]].position.y
         self.xarm7.set_pose_target(pose_goal)
         plan_success, traj, planning_time, error_code = self.xarm7.plan()
         self.ExecutePlan(traj)
@@ -59,13 +75,14 @@ class PNPDemo(object):
             gripper_values[0] = 0.5
         self.gripper.go(gripper_values, wait=True)
     
-    def lineMotion(self, direction):
+    def lineMotion(self, direction, cube):
         current_pose = self.xarm7.get_current_pose().pose
+        z = [0.04, 0.07]
         waypoints = []
-        if direction == "down": 
-            current_pose.position.z = 0.03
-        elif direction == "down2": 
-            current_pose.position.z = 0.05
+        if direction == "down1": 
+            current_pose.position.z = 0.02
+        elif direction == "down2":
+            current_pose.position.z = z[cube]
         elif direction == "up": 
             current_pose.position.z = 0.3
         waypoints.append(current_pose)
@@ -91,31 +108,45 @@ class PNPDemo(object):
 
 
 def main():
-    global markers
-
     move = PNPDemo()
     rospy.loginfo("Subscribing to ar_pose_marker")
     rospy.Subscriber("/ar_tf_markers", AlvarMarkers, callback)
 
     while not rospy.is_shutdown():
         inp = input("============ Press `Enter` to start Xarm7 movement")
-        if inp == "stop": break
-        if len(list(markers.keys())) == 4:
-            for id in range(len(cubes_ids)):
-                move.Xarm7ToStart()
-                move.Gripper("open")
-                move.Xarm7ToObject(id)
-                move.lineMotion("down")
-                move.Gripper("close")
-                move.lineMotion("up")
-                move.Xarm7ToStorage(id)
-                move.lineMotion("down")
-                move.Gripper("open")
-                move.lineMotion("up")
-                move.Xarm7ToStart()
-            markers = dict()
+        if inp == "stop": 
+            break
+        if inp == "start":
+            move.Xarm7ToStart()
+            move.Gripper("open")
+            break
+
+        move.Xarm7ToStart()
+        move.Gripper("open")
+        if len(found_markers) == len(marker_ids):
+            for box_id in list(markers.keys()):
+                rospy.loginfo('Collecting cubes to box: %s', str(box_id))
+                cube = 0
+                cubes_ids = list(markers[box_id].keys())
+                cubes_ids.remove(box_id)
+                for cube_id in cubes_ids:
+                    rospy.loginfo('Going for cube: %s', str(cube_id))
+                    move.Xarm7ToStart()
+                    move.Gripper("open")
+                    move.movexArm7(box_id, cube_id)
+                    move.lineMotion("down1", cube)
+                    move.Gripper("close")
+                    move.lineMotion("up", cube)
+                    move.movexArm7(box_id, box_id)
+                    move.lineMotion("down2", cube)
+                    move.Gripper("open")
+                    move.lineMotion("up", cube)
+                    move.Xarm7ToStart()
+                    cube += 1
+                    if rospy.is_shutdown():break
+                if rospy.is_shutdown():break
         else:
-            rospy.loginfo('Not all markers are detected.')
+            rospy.loginfo('Not all markers are detected. Found markers: %s', str(found_markers))
 
 if __name__ == '__main__':
     try:
